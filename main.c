f2412adec82f002161977722cc2b8dd21a0169cc
philipheferreira <49068956+philipheferreira@users.noreply.github.com>
2021-02-02 10:42:41 -0300

    incluindo bibliotecas e acentuação
    para ganhar o gás

---
+++ main.c
@@ -0,0 +1,16 @@
+#include<stdio.h>
+#include<stdlib.h>
+#include<locale.h>
+
+int main(){ 
+
+setlocale(LC_ALL, "");
+
+int menu;
+
+
+printf("\t\tBem vindo ao estacionamento do Philiphe");
+
+
+system("pause");
+}
