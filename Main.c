7ef8598e887f781fa9ea39af6a5d07fc20118a37
philipheferreira <49068956+philipheferreira@users.noreply.github.com>
2021-02-16 09:40:21 -0300

    Dados clientes
    atualização referente a coleta de dados dos clientes = primeiro nome, ultimo nome e tipo do carro. Ainda implementar referente ao carro. Se irei adicionar uma variável char e um comando do tipo escolha para repassar os dados para essa variável.

--- Main.c
+++ Main.c
@@ -8,9 +8,32 @@ setlocale(LC_ALL, "");
 
 int menu;
 
-
+struct cliente{ 
+char primeiroNome[20];
+char ultimoNome[20];
+int carro;
+}A;  
+  
 printf("\t\tBem vindo ao estacionamento do Philiphe");
+printf("Bom dia, qual o seu primeiro nome?\n");
+scanf("%S", &A.primeiroNome);
+  
+printf("Qual seu ultimo nome?\n");
+scanf("%S", &A.ultimoNome);  
+
+system("cls");
+
+printf("Bom dia senhor %i %i, gostariamos de saber qual a marca do seu carro com as opções a baixo: ", A.primeiroNome, A.ultimoNome);
+printf("1-Onix.\n2-HB20.\n3-Gol.4-KA.\n5-Argo.\n6-Kwid.\n7-Mobi.\n8-Polo.\n9-Sandeiro.\n10-Uno.");
+scanf("%i", &A.carro);
 
 
+printf("-------------------------------------------------------------------------------------------------------------------");
+printf("||                                      Ficha de informações                                                     ||");
+printf("||  Nome: %S %S                                                                                                  ||", A.primeiroNome, A.ultimoNome);
+printf("||  Tipo do carro:                                                                                               ||");
+printf("||                                                                                                               ||");
+printf("||                                                                                                               ||");  
+  
 system("pause");
 }
